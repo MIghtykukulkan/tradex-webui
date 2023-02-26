d4d1a0fe4e9232d47a599ffadb40240c76c6ccf7
Umashankar Somasekar <usomasekar@saisoftek.net>
2023-02-26 19:35:25 +0530

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>Welcome to my ReactJS application.</p>
+    </div>
+  );
+}
+
+export default App;
